f6815c57656a9d6f806484a52b47babebc32c0a7
Hsuchiahui <a22923509@gmail.com>
2018-07-16 13:22:28 +0800

    rename

--- Lesson03-Class/ex2.py
+++ Lesson03-Class/ex2.py
@@ -1,17 +1,19 @@
 import math
 
 class Map:
-    def __init__(self,n,p): #建立instance object時，自動執行。
+    def set_map(self,n):
         self.map_list = [['*' for i in range(n)] for j in range(n)]
         self.map_size = n 
 
-        if (self.map_size%2) == 1 and p==1:        
-            center = math.floor(self.map_size/2) 
+    def set_pattern(self,p):
+        if (self.map_size%2) == 1 and p==1:
+            center = math.floor(self.map_size/2)  #無條件捨取法
             self.map_list[center][center-1] = 0
             self.map_list[center-1][center-1] = 0
             self.map_list[center-1][center] = 0
             self.map_list[center-1][center+1] = 0
             self.map_list[center+1][center] = 0
+             
 
     def display(self):
         for i in range(self.map_size):
@@ -21,9 +23,10 @@ class Map:
 
 n= int(input("Please enter number："))
 p = int(input("Please enter number 1 to set pattern：")) 
-map = Map(n,p)
-
+map = Map()
+map.set_map(n)
 map.display()
-
-
+print()
+map.set_pattern(p)
+map.display()
 
